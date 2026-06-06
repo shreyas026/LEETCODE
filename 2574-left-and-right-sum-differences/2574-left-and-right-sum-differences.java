f7fa8f0339e7b503fb62d30610f420564bfbeea7
shreyas026 <shreyassivakumarcse@gmail.com>
2026-06-06 13:05:25 +0530

    problem 2574 solved

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -0,0 +1,67 @@
+// ======================================
+// 2574. Left and Right Sum Differences
+// ======================================
+// Difficulty: Easy
+//
+// You are given a **0-indexed** integer array `nums` of size `n`.
+//
+// Define two arrays `leftSum` and `rightSum` where:
+//
+// 	
+// - `leftSum[i]` is the sum of elements to the left of the index `i` in the array `nums`. If there is no such element, `leftSum[i] = 0`.
+// 	
+// - `rightSum[i]` is the sum of elements to the right of the index `i` in the array `nums`. If there is no such element, `rightSum[i] = 0`.
+//
+// Return an integer array `answer` of size `n` where `answer[i] = |leftSum[i] - rightSum[i]|`.
+//
+//  
+//
+// **Example 1:**
+//
+// **Input:** nums = [10,4,8,3]
+// **Output:** [15,1,11,22]
+// **Explanation:** The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+// The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+//
+// ```
+//
+// **Example 2:**
+//
+// **Input:** nums = [1]
+// **Output:** [0]
+// **Explanation:** The array leftSum is [0] and the array rightSum is [0].
+// The array answer is [|0 - 0|] = [0].
+//
+// ```
+//
+//  
+//
+// **Constraints:**
+//
+// 	
+// - `1 &lt;= nums.length &lt;= 1000`
+// 	
+// - `1 &lt;= nums[i] &lt;= 10^5`
+//
+// ====== SOLUTION ======
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            answer[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+        
+        return answer;
+    }
+}
