04810b1ac8fd5cb8371b681bc3f30d4192604498
SHREYAS S <shreyassivakumarcse@gmail.com>
2026-08-30 14:55:14 +0530

    0485-max-consecutive-ones solved

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,38 +1,3 @@
-// ===========================
-// 485. Max Consecutive Ones
-// ===========================
-// Difficulty: Easy
-//
-// Given a binary array `nums`, return *the maximum number of consecutive *`1`*&#39;s in the array*.
-//
-//  
-//
-// **Example 1:**
-//
-// **Input:** nums = [1,1,0,1,1,1]
-// **Output:** 3
-// **Explanation:** The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
-//
-// ```
-//
-// **Example 2:**
-//
-// **Input:** nums = [1,0,1,1,0,1]
-// **Output:** 2
-//
-// ```
-//
-//  
-//
-// **Constraints:**
-//
-// 	
-// - `1 &lt;= nums.length &lt;= 10^5`
-// 	
-// - `nums[i]` is either `0` or `1`.
-//
-// ====== SOLUTION ======
-
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count = 0, max = 0;
